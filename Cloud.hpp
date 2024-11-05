22db0f9c8083f017bea04d311445f5c2da04dee6
Ron <113129998+RonB8@users.noreply.github.com>
2024-11-05 20:17:31 +0200

    Update Cloud.hpp

--- Cloud.hpp
+++ Cloud.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "includeAll.hpp"
+#include "commonInc.hpp"
 
 
 #include <files.h>
 #include "iostream"
 #include "Server.hpp"
 #include "Request.hpp"
-#include "Response.hpp"
+#include "ResponseParser.hpp"
 #include <filesystem>
 #include "CryptoManager.hpp"
 #include "ByteUtils.hpp"
@@ -16,16 +16,25 @@
 
 unsigned long checkSum(const std::string& fname);
 
+class Server;
 
 class Cloud {
-    Server server;
-public:
+
+private:
+    std::unique_ptr<Server> server;
+
+public:    
     Cloud();
+    ~Cloud();
     Response registerName(const std::string& name);
     Response login(const std::string& name, const std::vector<uint8_t>& ID);
     Response sendPublicKey(const std::string& name, const std::vector<uint8_t>& ID, const std::vector<uint8_t>& public_key);
-    //Response sendFile(const std::vector<uint8_t>& ID, const std::filesystem::path& filePath, const AesKey& aesKey); $$$$$$$$
     Response sendFile(const std::vector<uint8_t>& ID, const std::filesystem::path& filePath, const CryptoManager& cryptoManager);
-    std::vector<uint8_t> sendFileByPackets(const std::vector<uint8_t>& content, const uint8_t IDArr[], uint32_t origFileSize, const std::string& fileName);
+    
+
+
+
+    std::vector<uint8_t> sendFileByPackets(const std::vector<uint8_t>& content, const std::vector<uint8_t>& IDArr, uint32_t origFileSize, const std::string& fileName);
 };
+
 
