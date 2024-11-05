6025798fbad90821e18d9e5fd53552ab4a0126b3
Ron <113129998+RonB8@users.noreply.github.com>
2024-11-05 20:29:35 +0200

    Update ByteFuncs.py

--- ByteFuncs.py
+++ ByteFuncs.py
@@ -1,10 +1,15 @@
 # Given an array insert to the end the number as nB representation
-def push_as_n_bytes(arr, number, nB):
+def push_as_n_bytes(arr: bytes, number, num_bytes: int) -> None:
     if isinstance(number, int):
-        n = nB - 1
+        n = num_bytes - 1
         while n >= 0:
             arr.append((number >> (n * 8)) & 0xFF)
             n -= 1
     else:
         for var in number:
             arr.append(var)
+
+
+def unpad(data: bytes) -> bytes:
+    padding_len = data[-1]  # הערך האחרון הוא גודל הריפוד
+    return data[:-padding_len]
