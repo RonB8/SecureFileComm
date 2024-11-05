58383a8a3b5b31cdafec32e4bf1aa4a69f8cd479
Ron <113129998+RonB8@users.noreply.github.com>
2024-11-05 20:30:14 +0200

    Update ByteUtils.hpp

--- ByteUtils.hpp
+++ ByteUtils.hpp
@@ -7,36 +7,6 @@
 
 
 
-//@@@@@@@@@@@@@@@@@@@@ temporary?
-inline [[maybe_unused]] std::string maskZero(const std::string& str) {
-    size_t n = str.length();
-    std::string res(255 - n, '\0');
-    return res + str;
-}
-
-
-//@@@@@@@@@@@@@@@@@@@@ temporary
-inline void printVector(const std::vector<uint8_t>& vec) {
-    int index = 0;
-    for (auto var : vec) {
-        if (var != 0) {
-            // הדפסת האינדקס
-            printf("[%d] = ", index);
-
-            // הדפסה עשרונית
-            std::cout << std::dec << static_cast<int>(var) << " : ";
-
-            // הדפסה הקסדצימלית
-            std::cout << std::hex << static_cast<int>(var & 0xFF) << " : ";
-
-            // הדפסת הערך המקורי
-            std::cout << static_cast<int>(var) << std::endl;
-        }
-        index++;
-    }
-}
-
-
 inline std::vector<uint8_t> strToBytes(const std::string& idStr) {
     std::vector<uint8_t> bytes;
     for (char i : idStr)
@@ -44,9 +14,15 @@ inline std::vector<uint8_t> strToBytes(const std::string& idStr) {
     return bytes;
 }
 
+//As little endian
 inline void pushUintToBuffer(std::vector<uint8_t>& buffer, uint32_t value, size_t byteSize) {
-    for (size_t i = byteSize - 1; i >= 0; --i) {
-        buffer.push_back(static_cast<uint8_t>(value >> (8 * i)));
+    if (byteSize == 0) {
+        std::cerr << "Error in pushUintToBuffer. Byte size must be poisitive.";
+    }
+    else {
+        for (int i = 0; i < byteSize; i++) {
+            buffer.push_back(static_cast<uint8_t>(value >> (8 * i)));
+        }
     }
 }
 
