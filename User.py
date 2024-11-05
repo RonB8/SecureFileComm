8c154cc7bc3f44407bd44355eb9204d5c8614fca
Ron <113129998+RonB8@users.noreply.github.com>
2024-11-05 20:14:06 +0200

    Update User.py

--- User.py
+++ User.py
@@ -1,69 +1,110 @@
 import uuid
-
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-
-# from main import users_list
+from typing import Any
+from CryptoManager import CryptoManager
 
 
 class User:
-    def __init__(self, id: bytes, name: str):
+    """
+   The User class represents a client, containing information
+   such as the user's ID, name, and cryptographic management. It handles
+   the storage of encrypted content and decrypted files associated with the user.
+
+   Attributes:
+       __id (bytes): A unique identifier for the user.
+       __name (str): The name of the user.
+       crypto_manager (CryptoManager): An instance of CryptoManager for managing encryption/decryption.
+       __encrypted_content (bytearray): Stores the encrypted content packets associated with the user.
+       __temp_file_content (Any): Temporarily holds content that may be decrypted and saved.
+       __decrypted_content (dict): A dictionary storing decrypted content with file names as keys.
+
+   Methods:
+       __init__(self, id: bytes, name: str, crypto_manager: CryptoManager):
+           Initializes the User instance with an ID, name, and a CryptoManager instance.
+
+       get_id(self) -> bytes:
+           Returns the unique identifier of the user.
+
+       get_name(self) -> str:
+           Returns the name of the user.
+
+       append_encrypted_packet(self, packet: bytearray) -> None:
+           Appends an encrypted packet to the user's encrypted content.
+
+       get_encrypted_content(self) -> bytearray:
+           Returns a copy of the user's encrypted content.
+
+       clear_encrypted_content(self) -> None:
+           Delete the stored encrypted content.
+
+       save_content_file(self, file_name: str, content: bytes) -> None:
+           Saves decrypted content associated with a given file name.
+
+       get_content_file(self, file_name: str) -> bytes:
+           Retrieves the decrypted content for the specified file name.
+
+       clear_content_file(self, file_name: str) -> None:
+           Delete the decrypted content associated with the specified file name.
+   """
+
+    def __init__(self, id: bytes, name: str, crypto_manager:CryptoManager):
         self.__id = id
         self.__name = name
-        self.public_key = None
-        self.aes_cipher = None
-        self.aes_key = None
-        self.temp_encrypted_content = bytearray([])
-        self.temp_content = None
+        self.crypto_manager = crypto_manager
+        self.__encrypted_content = bytearray([])
+        self.__temp_file_content = None
+        self.__decrypted_content = {}
 
     def get_id(self) -> bytes:
         return self.__id
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__name
 
-    def generate_aes_key(self):
-        self.aes_key = get_random_bytes(32)
+    def append_encrypted_packet(self, packet:bytearray) -> None:
+        self.__encrypted_content += packet[:]
 
-        #For the purpose of the project, the client assumes that the IV
-        #is always filled with 0, although this is not a sure thing
-        iv = bytearray([0 for _ in range(16)])
+    def get_encrypted_content(self) -> bytearray:
+        return self.__encrypted_content[:]
 
-        self.aes_cipher = AES.new(self.aes_key, AES.MODE_CBC, iv)
-        return self.aes_key
+    def clear_encrypted_content(self) -> None:
+        self.__encrypted_content.clear()
 
-    def get_aes_key(self):
-        return self.aes_key
+    def save_content_file(self, file_name:str, content: bytes) -> None:
+        self.__decrypted_content[file_name] = content
 
-    def set_public_key(self, pub_key):
-        self.public_key = pub_key
+    def get_content_file(self, file_name:str) -> bytes:
+        return self.__decrypted_content[file_name]
 
-    def get_public_key(self):
-        return self.public_key
-
-    def append_to_temp_encrypted_content(self, content:bytearray):
-        self.temp_encrypted_content += content[:]
-
-    def get_temp_encrypted_content(self):
-        return self.temp_encrypted_content[:]
-
-    def set_temp_content(self, content):
-        self.temp_content = content
-
-    def get_temp_content(self):
-        return self.temp_content
-
-    def clear_temp_encrypted_content(self):
-        self.temp_encrypted_content.clear()
+    def clear_content_file(self, file_name:str) -> None:
+         del self.__decrypted_content[file_name]
 
 
-class Users:
+class UserRepository:
+    """
+    The UserRepository class manages the registration and retrieval of User instances.
+    It stores a list of registered users and ensures that user IDs are unique.
+
+    Attributes:
+        repository (list): A list containing registered User instances.
+        id_list (list): A list of unique user IDs to prevent duplicates.
+
+    Methods:
+        __init__(self):
+            Initializes the UserRepository instance.
+
+        register(self, name: str, crypto_manager: CryptoManager) -> User | None:
+            Adds a new user with a unique ID and name.
+
+        get_user(self, c_id: bytes) -> User | None:
+            Retrieves a user by their unique ID.
+    """
+
     def __init__(self):
-        self.users_list = []
+        self.repository = []
         self.id_list = []
 
-    def register(self, name: str):
-        if any(usr.get_name() == name for usr in self.users_list):
+    def register(self, name: str, crypto_manager:CryptoManager):
+        if any(usr.get_name() == name for usr in self.repository):
             print(f"The user {name} already exist")
             return None
 
@@ -72,17 +113,18 @@ class Users:
             new_id = uuid.uuid4()
 
         self.id_list.append(new_id)
-        new_user = User(new_id.bytes, name)
-        self.users_list.append(new_user)
+        new_user = User(new_id.bytes, name, crypto_manager)
+        self.repository.append(new_user)
         print(f"Registering {name}")
         return new_user
 
-    def get_user(self, c_id: bytes) ->User:
-        for usr in self.users_list:
+    def get_user(self, c_id: bytes) -> Any | None:
+        for usr in self.repository:
             curr_id = usr.get_id()
             if c_id == curr_id:
                 return usr
 
-        return None
+        #If the user does not exist
+        raise Exception(f'There is no user with ID: {c_id}')
 
 
